43b4ac5216c3d43e6cae49aabc543a8ec7fa5c0c
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2018-12-30 22:29:08 -0300

    adding thumbnail_image and some styles.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import styles from './styles/AlbumDetailStyles';
 
 const AlbumDetail = props => {
-	const { album } = props;
+	const {
+		album: { title, artist, thumbnail_image }
+	} = props;
+	const { thumbnailStyle, wrapperInformations } = styles;
 	return (
 		<Card>
 			<CardSection>
-				<Text>{album.title}</Text>
+				<View>
+					<Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
+				</View>
+				<View style={wrapperInformations}>
+					<Text>{title}</Text>
+					<Text>{artist}</Text>
+				</View>
 			</CardSection>
 		</Card>
 	);
---
+++ src/components/styles/AlbumDetailStyles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	wrapperInformations: {
+		justifyContent: 'space-around'
+	},
+	wrapperImage: {
+		flex: 1
+	},
+	thumbnailStyle: {
+		width: 50,
+		height: 50
+	}
+});
